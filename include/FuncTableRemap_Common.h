107616897b97934314ead689e385edcf0913fd98
Maeiky <Maeiky@gmail.com>
2020-01-28 21:49:31 -0500

    Completed TLS

--- include/FuncTableRemap_Common.h
+++ include/FuncTableRemap_Common.h
@@ -1,20 +1,36 @@
 
+//#define Use_Custom_ThreadStorage
 #ifdef Use_Custom_ThreadStorage
 //DWORD  TlsAlloc();										 //If the function succeeds, the return value is a TLS index. The slots for the index are initialized to zero.
 //BOOL   TlsSetValue( DWORD  dwTlsIndex, LPVOID lpTlsValue); //If the function succeeds, the return value is nonzero.
 //LPVOID TlsGetValue(DWORD dwTlsIndex); 					 //Each thread of a process has its own slot for each TLS index
 
+void** aTlsNewMem;
 DWORD WINAPI My_TlsAlloc(void){
-    _EXE_LOADER_DEBUG(3, "TlsAlloc non implemente!", "TlsAlloc not implemented!");
-	return 0;
+   // _EXE_LOADER_DEBUG(3, "TlsAlloc non implemente!", "TlsAlloc not implemented!");	//return 0;
+	static int _nIndex = 0;
+	static int _nMax = 0;
+	if(_nIndex >= _nMax){ //Realloc
+		int _nNewSize = _nMax*2 + 5; 
+		 void** _aNewMem = (void**)calloc(_nNewSize, sizeof(void*));
+		 memcpy(_aNewMem, aTlsNewMem, _nMax*sizeof(void*) );
+		 free(aTlsNewMem);
+		 aTlsNewMem = _aNewMem;
+		 _nMax = _nNewSize;
+	}
+	_nIndex++;
+	return _nIndex-1;
 }
 BOOL  WINAPI My_TlsSetValue(  DWORD dwTlsIndex, _In_opt_ LPVOID lpTlsValue){
-    _EXE_LOADER_DEBUG(3, "TlsSetValue non implemente!", "TlsSetValue not implemented!");
-    return false;
+ // _EXE_LOADER_DEBUG(3, "TlsSetValue non implemente!", "TlsSetValue not implemented!"); return false;
+ //  printf("\n Set: %d", dwTlsIndex);
+   aTlsNewMem[dwTlsIndex] = lpTlsValue;
+   return true;
 }
 LPVOID WINAPI My_TlsGetValue(  DWORD dwTlsIndex){
-    _EXE_LOADER_DEBUG(3, "TlsGetValue non implemente!", "TlsGetValue not implemented!");
-    return (LPVOID)0;
+  //  _EXE_LOADER_DEBUG(3, "TlsGetValue non implemente!", "TlsGetValue not implemented!");  return (LPVOID)0;
+  // printf("\n Get: %d", dwTlsIndex);
+   return aTlsNewMem[dwTlsIndex];
 }
 #else
 /*
